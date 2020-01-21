c541cbff9e564855582faf3bc130b577b67d6bff
rhyanz46 <rianariansaputra@gmail.com>
2020-01-21 10:04:01 +0800

    add firebase

--- core/config.py
+++ core/config.py
@@ -2,6 +2,12 @@ import os
 from os.path import join
 from dotenv import load_dotenv
 
+
+import firebase_admin
+from firebase_admin import credentials
+from os import getcwd
+
+
 dotenv_path = join(os.getcwd(), '.env')
 load_dotenv(dotenv_path)
 
@@ -11,6 +17,12 @@ DATABASE_HOST = os.environ.get("DATABASE_HOST")
 DATABASE_USER = os.environ.get("DATABASE_USER")
 DATABASE_PASSWORD = os.environ.get("DATABASE_PASSWORD")
 DATABASE_NAME = os.environ.get("DATABASE_NAME")
+
+
+firebase_credential_file = getcwd() + str('/credentials/halalbeehalal-firebase-adminsdk-r8nxj-4de9d7a329.json')
+firebase_credential = credentials.Certificate(firebase_credential_file)
+firebase_app = firebase_admin.initialize_app(firebase_credential)
+
 
 config = {
     'SQLALCHEMY_DATABASE_URI': 'mysql+pymysql://{}:{}@{}:3307/{}'.format(
